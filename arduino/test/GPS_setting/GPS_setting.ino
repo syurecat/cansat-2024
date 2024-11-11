755bd6f11758761cc30329301d756d98e3acd743
syurecat <syurecat0@gmail.com>
2024-11-11 17:33:48 +0900

    fix checksum

--- arduino/test/GPS_setting/GPS_setting.ino
+++ arduino/test/GPS_setting/GPS_setting.ino
@@ -1,19 +1,21 @@
 #include <SoftwareSerial.h>
 
+SoftwareSerial GpsSerial(PIN_GPS_TX, PIN_GPS_RX);
+
 void setup() {
     Serial.begin(115200);
-    mySerial.begin(9600);
-    mySerial.println("$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28");
+    GpsSerial.begin(9600);
+    GpsSerial.println("$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28");
     Serial.println("Ready");
 }
 void loop() {
     // 作りかけだよ！
 }
 
-int getChecksum(data) {
-    checksum = 0
-    raw_data.unpack("C*").each do |byte|
-        checksum += byte
-    end
-    return raw_data.unpack("C*").push(checksum / 100)
+int getChecksum(String data) {
+    byte checksum = 0;
+    for (int i = 0; i < data.length(); i++) {
+        checksum += data[i];
+    }
+    return checksum
 }
