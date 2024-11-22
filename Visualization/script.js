8cc44e4db868e01695032bfe77653bf0731e832e
syurecat <syurecat0@gmail.com>
2024-11-22 14:18:39 +0900

    add OrbitControls

--- Visualization/script.js
+++ Visualization/script.js
@@ -3,13 +3,14 @@
 import * as THREE from "three";
 import { GLTFLoader } from "GLTFLoader";
 import Stats from "stats";
+import { OrbitControls } from "orbitcontrols";
 
 const CANDATA = "./cansat-3dData/cansatafull.gltf"
 const W_WIDTH  = window.innerWidth; // ブラウザの横サイズ
 const W_HEIGHT = window.innerHeight;// ブラウザの縦サイズ
 const W_ASPECT = window.innerWidth / window.innerHeight;// アスペクト比
 const W_RATIO  = window.devicePixelRatio;// ピクセル比
-let camera, scene, renderer, stats, cube, model;// カメラ、シーン、レンダラー、立方体
+let camera, scene, renderer, stats, controls, cube, model;// カメラ、シーン、レンダラー、立方体
 
 window.onload = ()=>{
 	//カメラ
@@ -17,9 +18,9 @@ window.onload = ()=>{
 	camera.position.set(0, 5, 0);
 	//シーン
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color('#00bfff');
+	scene.background = new THREE.Color('#00ff00');
 	//ヘルパー
-	let gridhelper = new THREE.GridHelper();
+	let gridhelper = new THREE.GridHelper( 10, 50, '#ff0000');
 	scene.add(gridhelper);
 	//ライト
 	// let ambLight = new THREE.AmbientLight(0x333333);
@@ -37,6 +38,9 @@ window.onload = ()=>{
 
 	stats = Stats();
 	document.body.appendChild(stats.dom);
+
+	controls = new OrbitControls( camera, renderer.domElement );
+	controls.update();
 
 	//テスト
 	// const geometry = new THREE.BoxGeometry(10,10,10);
@@ -66,6 +70,7 @@ function animate(){
 		// model.rotation.y += 0.01;
 	}
 	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	controls.update();
 	stats.update();
 	renderer.render(scene, camera);// レンダリング
 	requestAnimationFrame(animate);// 更新
