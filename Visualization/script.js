b1e1c8558f525272dd381b5503c28e48a1a858f8
syurecat <syurecat0@gmail.com>
2024-11-22 12:19:16 +0900

    fix all script

--- Visualization/script.js
+++ Visualization/script.js
@@ -8,14 +8,18 @@ const W_WIDTH  = window.innerWidth; // ブラウザの横サイズ
 const W_HEIGHT = window.innerHeight;// ブラウザの縦サイズ
 const W_ASPECT = window.innerWidth / window.innerHeight;// アスペクト比
 const W_RATIO  = window.devicePixelRatio;// ピクセル比
-let camera, scene, renderer, cube;// カメラ、シーン、レンダラー、立方体
+let camera, scene, renderer, cube, model;// カメラ、シーン、レンダラー、立方体
 
 window.onload = ()=>{
 	//カメラ
-	camera = new THREE.PerspectiveCamera(50, W_ASPECT, 1, 1000);
-	camera.position.set(0, 0, 50);
+	camera = new THREE.PerspectiveCamera(60, W_ASPECT, 0.1, 1000);
+	camera.position.set(0, 5, 0);
 	//シーン
 	scene = new THREE.Scene();
+	scene.background = new THREE.Color('#00bfff');
+	//ヘルパー
+	let gridhelper = new THREE.GridHelper();
+	scene.add(gridhelper);
 	//ライト
 	// let ambLight = new THREE.AmbientLight(0x333333);
 	// scene.add(ambLight);
@@ -31,28 +35,33 @@ window.onload = ()=>{
 	div.appendChild(renderer.domElement);
 
 	//テスト
-	const geometry = new THREE.BoxGeometry(10,10,10);
-	const material = new THREE.MeshBasicMaterial({color:0x00ff00});
-	cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
+	// const geometry = new THREE.BoxGeometry(10,10,10);
+	// const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+	// cube = new THREE.Mesh(geometry, material);
+	// scene.add(cube);
 	
 	//配置
-    // const loader = new GLTFLoader();
-    // loader.load(CANDATA, function (gltf) {
-    //     const model = gltf.scene;
-    //     scene.add(model);
+    const loader = new GLTFLoader();
+    loader.load(CANDATA, function (gltf) {
+        model = gltf.scene;
+        scene.add(model);
 
-    //     model.scale.set(1, 1, 1);  // サイズ調整
-    // }, undefined, function (error) {
-    //     console.error('Model loading error:', error);
-    // });
+        model.scale.set(20, 20, 20);  // サイズ調整
+		model.position.set(0, 0, 0);
+    }, undefined, function (error) {
+        console.error('Model loading error:', error);
+    });
 	//アニメーション
 	animate();
 }
 
 function animate(){
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	//読み込み待ち
+	if (model) {
+		// model.rotation.x += 0.01;
+		// model.rotation.y += 0.01;
+	}
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
 	renderer.render(scene, camera);// レンダリング
 	requestAnimationFrame(animate);// 更新
 }
