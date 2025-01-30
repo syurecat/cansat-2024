eb49eec95827a04f1fb73cc96b5d966bd0c444ad
syurecat <syurecat0@gmail.com>
2025-01-31 00:53:12 +0900

    add server docker

---
+++ Visualization/app/main.js
@@ -0,0 +1,101 @@
+import { WebSocketServer } from 'ws';
+import {InfluxDB, Point} from '@influxdata/influxdb-client'
+
+const influxDB = new InfluxDB({
+    http://influxdb/,
+    YOUR_API_TOKEN
+})
+const writeApi = influxDB.getWriteApi(
+    YOUR_ORG,
+    YOUR_BUCKET
+)
+const express = require('express');
+const app = express();
+app.use(express.json())
+const wrap = fn => (...args) => fn(...args).catch(args[2])
+const SENSER_TYPES = ["ACC", "GPU", "GYR", "MAG", "BME"];
+
+const server = app.listen(4080, () => {
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+const wss = new WebSocketServer({ server });
+
+function authenticate(req, res, next) {
+    const token = req.body.token;
+    if(!token) {
+        res.status(401).send('Unauthorized')
+    }else if (token === AUTH_TOKEN) {
+        next();
+    } else {
+        res.status(403).send('Forbidden');
+    }
+}
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
+
+app.post('/update', authenticate, wrap(async (req, res, next) => {
+        console.log('Authenticated POST request received:', req.body);
+        if (!req.body.type || !req.body.name || !req.body.data) {
+            res.status(400).json({ massage: "Bad Request" });
+        } else if (!SENSER_TYPES.includes(type)) {
+            res.status(421).json({ massage: "Misdirected Request" });
+        } else {
+            next();
+        }
+    }), wrap(async (req, res, next) => {
+        try {
+            const point = new Point(type)
+                .tag("sensor", req.body.name);
+
+            for (const key in data) {
+                if (typeof data[key] === "number") {
+                    point.floatField(key, data[key]);
+                }
+            }
+            
+            if (!req.body.stre) {
+                point.floatField("stre", stre);
+            }
+            writeApi.writePoint(point);
+            await writeApi.flush();
+            res.status(200).json({ massage: "success" });
+            if (req.body.type == "IMU") {
+                clients.forEach(client => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.json(req.body.data);
+                    }
+                });
+            }
+        } catch (error) {
+            console.error('Error Updating:', error);
+            res.status(500).json({ massage: "Failed to Update."});
+        }
+    })
+);
+
+app.all('/update', wrap(async (req, res, next) => {
+    res.status(405).json({ massage: "Method Not Allowed" });
+}));
+
+app.all('/teapot', wrap(async (req, res, next) => {
+    res.status(418).send("I'm a teapot" );
+}));
+
+app.all("*", wrap(async (req, res, next) => {
+    res.status(404).send("Not Found");
+}));
+
+app.use(async (err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send("Internal Server Error");
+});
+  
---
+++ Visualization/app/script.js
@@ -0,0 +1,91 @@
+//参考 https://zenn.dev/sdkfz181tiger/books/735e854bee9fc9/viewer/
+
+import * as THREE from "three";
+import { GLTFLoader } from "GLTFLoader";
+import Stats from "stats";
+import { OrbitControls } from "orbitcontrols";
+
+const CANDATA = "./cansat-3dData/cansatafull.gltf"
+const CANDATA2 = "./cansat-3dData/Body.gltf"
+const W_WIDTH  = window.innerWidth; // ブラウザの横サイズ
+const W_HEIGHT = window.innerHeight;// ブラウザの縦サイズ
+const W_ASPECT = window.innerWidth / window.innerHeight;// アスペクト比
+const W_RATIO  = window.devicePixelRatio;// ピクセル比
+let camera, scene, renderer, stats, controls, cube, model, model2;// カメラ、シーン、レンダラー、立方体
+
+window.onload = ()=>{
+	//カメラ
+	camera = new THREE.PerspectiveCamera(60, W_ASPECT, 0.1, 1000);
+	camera.position.set(0, 5, 0);
+	//シーン
+	scene = new THREE.Scene();
+	scene.background = new THREE.Color('#00ff00');
+	//ヘルパー
+	let gridhelper = new THREE.GridHelper( 20, 100, '#ff0000');
+	scene.add(gridhelper);
+	//ライト
+	// let ambLight = new THREE.AmbientLight(0x333333);
+	// scene.add(ambLight);
+	let dirLight = new THREE.DirectionalLight(0xffffff, 1);
+	dirLight.position.set(10,10,10);
+	scene.add(dirLight);
+
+	let helper = new THREE.DirectionalLightHelper( dirLight, 5 );
+	scene.add( helper );
+	//レンダラー
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setPixelRatio(W_RATIO);// ピクセル比
+	renderer.setSize(W_WIDTH, W_HEIGHT);
+	//配置
+	let div = document.getElementById("three");
+	div.appendChild(renderer.domElement);
+
+	stats = Stats();
+	document.body.appendChild(stats.dom);
+
+	controls = new OrbitControls( camera, renderer.domElement );
+	controls.update();
+
+	//テスト
+	// const geometry = new THREE.BoxGeometry(10,10,10);
+	// const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+	// cube = new THREE.Mesh(geometry, material);
+	// scene.add(cube);
+	
+	//配置
+    const loader = new GLTFLoader();
+    loader.load(CANDATA, function (gltf) {
+        model = gltf.scene;
+        scene.add(model);
+
+        model.scale.set(20, 20, 20);  // サイズ調整
+		model.position.set(0, 0, 0);
+    }, undefined, function (error) {
+        console.error('Model loading error:', error);
+    });
+    const loader2 = new GLTFLoader();
+    loader2.load(CANDATA2, function (gltf) {
+        model2 = gltf.scene;
+        scene.add(model2);
+
+        model2.scale.set(20, 20, 20);  // サイズ調整
+		model2.position.set(0, 0, 0);
+    }, undefined, function (error) {
+        console.error('Model loading error:', error);
+    });
+	//アニメーション
+	animate();
+}
+
+function animate(){
+	//読み込み待ち
+	if (model) {
+		// model.rotation.x += 0.01;
+		// model.rotation.y += 0.01;
+	}
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	controls.update();
+	stats.update();
+	renderer.render(scene, camera);// レンダリング
+	requestAnimationFrame(animate);// 更新
+}
