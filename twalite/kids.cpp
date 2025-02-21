1b221a34f033eb08d7bbbd9a055bf7ab358b2aa8
Masa <117551105+pensol@users.noreply.github.com>
2025-02-21 10:56:53 +0900

    add

--- twalite/kids.cpp
+++ twalite/kids.cpp
@@ -152,7 +152,7 @@ void wakeup() {
 	Wire.begin();
 
 	Serial	<< crlf << "--- wake up ---";
-	Serial << crlf << sns_bme280.get_temp() << sns_bme280.get_humid() << sns_bme280.get_press();
+	Serial << crlf << sns_bme280.get_temp() "," sns_bme280.get_humid() "," sns_bme280.get_press();
 
 	State = STATE::INIT; // go into INIT state in the loop()
 }
